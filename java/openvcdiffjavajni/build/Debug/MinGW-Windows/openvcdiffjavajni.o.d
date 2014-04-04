81d36dc81b13b271a1cea2521a13c77f3131be23
Atul Vinayak <su.atul.vi@gmail.com>
2014-04-04 20:44:37 +0530

    Added openvcdiff-java: JNI  to open-vcdiff

---
+++ java/openvcdiffjavajni/build/Debug/MinGW-Windows/openvcdiffjavajni.o.d
@@ -0,0 +1,21 @@
+build/Debug/MinGW-Windows/openvcdiffjavajni.o: openvcdiffjavajni.cpp \
+  ../../../../../Program\ Files/Java/jdk1.7.0_45/include/jni.h \
+  ../../../../../Program\ Files/Java/jdk1.7.0_45/include/win32/jni_md.h \
+  g:/Lab/open-vcdiff-0.8.3/src/google/vcencoder.h \
+  g:/Lab/open-vcdiff-0.8.3/src/google/format_extension_flags.h \
+  g:/Lab/open-vcdiff-0.8.3/src/google/output_string.h \
+  g:/Lab/open-vcdiff-0.8.3/src/google/vcdecoder.h openvcdiffjavajni.h
+
+../../../../../Program\ Files/Java/jdk1.7.0_45/include/jni.h:
+
+../../../../../Program\ Files/Java/jdk1.7.0_45/include/win32/jni_md.h:
+
+g:/Lab/open-vcdiff-0.8.3/src/google/vcencoder.h:
+
+g:/Lab/open-vcdiff-0.8.3/src/google/format_extension_flags.h:
+
+g:/Lab/open-vcdiff-0.8.3/src/google/output_string.h:
+
+g:/Lab/open-vcdiff-0.8.3/src/google/vcdecoder.h:
+
+openvcdiffjavajni.h:
