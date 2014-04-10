7d31b13efc8265259f35ad54a035c32b01f40459
Atul Vinayak <su.atul.vi@gmail.com>
2014-04-10 14:30:36 +0530

    preparing for multiple entities

--- java/neoba.dsync.client/src/neoba/dsync/client/Editor.java
+++ java/neoba.dsync.client/src/neoba/dsync/client/Editor.java
@@ -47,7 +47,7 @@ public class Editor extends javax.swing.JFrame {
         syncButton = new javax.swing.JButton();
         hashdocLabel = new javax.swing.JLabel();
         hashdictLabel = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -71,7 +71,7 @@ public class Editor extends javax.swing.JFrame {
 
         hashdictLabel.setText("Dictionary: ");
 
-        jLabel3.setText("Neoba Dsync Client");
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,8 +82,8 @@ public class Editor extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(syncButton))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(hashdocLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -97,7 +97,7 @@ public class Editor extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(syncButton)
-                    .addComponent(jLabel3))
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -238,7 +238,7 @@ public class Editor extends javax.swing.JFrame {
     private javax.swing.JTextArea DocArea;
     private javax.swing.JLabel hashdictLabel;
     private javax.swing.JLabel hashdocLabel;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton syncButton;
     // End of variables declaration//GEN-END:variables
